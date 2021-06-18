124cadfb269288dae7dde9393bf5fad4bc10c63f
markraley <mdraley2012@gmail.com>
2021-06-17 17:50:38 -0700

    removed copyright notice from test code

--- vpbuf/tests/vectors/nodejs/src/persist.js
+++ vpbuf/tests/vectors/nodejs/src/persist.js
@@ -1,7 +1,3 @@
-// Versioned Polymorphic Buffers (vpbuf) - data interchange format
-//    Copyright Mark Raley, All Rights Reserved, https://github.com/markraley
-//    Software release is via MIT license (see project root for license file)
-
 module.exports = {
     Header: function(version = 1, test_name = "not set") {
         this.version = version;
--- vpbuf/tests/vectors/nodejs/src/read_context.js
+++ vpbuf/tests/vectors/nodejs/src/read_context.js
@@ -1,7 +1,3 @@
-// Versioned Polymorphic Buffers (vpbuf) - data interchange format
-//    Copyright Mark Raley, All Rights Reserved, https://github.com/markraley
-//    Software release is via MIT license (see project root for license file)
-
 const assert = require('chai').assert;
 "use strict";
 
