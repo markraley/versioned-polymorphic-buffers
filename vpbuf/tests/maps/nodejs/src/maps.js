fbde34d6440d7ed824bdeff27a34cc972bf40d25
markraley <mdraley2021@gmail.com>
2021-06-24 15:53:00 -0700

    added js maps_B test

--- vpbuf/tests/maps/nodejs/src/maps.js
+++ vpbuf/tests/maps/nodejs/src/maps.js
@@ -70,3 +70,61 @@ const out_dir = './out/';
 })();
 
 // ---------------------------------------------------------------------
+
+// ---------------------------------------------------------------------
+// maps_B -
+
+(function() {
+    var Test_maps_B = {
+        count: 10,
+        base: 50000,
+        version: -99,
+        test_name: 'maps_B',
+
+        // vars h and g are initialized and serialized to buffer, then file
+        // vars hh and gg are deserializations of the same
+
+        validate: function() {
+            assert.deepEqual(Test_maps_B.h, Test_maps_B.hh)
+            assert.deepEqual(Test_maps_B.bb, Test_maps_B.bb)
+        },
+
+        serialize: function() {
+            this.h = new persist.Header(this.version, this.test_name);
+            this.b = new persist.OuterB();
+
+            for (var i = 0; i < this.count; i++) {
+                var j = '' + (this.base + i)
+                this.b.lookup[j] = new persist.A(j, 'B-' + j)
+            }
+
+            write_context.buf_arr = [];
+            vp.write_Header(1, write_context, this.h);
+            vp.write_OuterB(1, write_context, this.b);
+
+            bytes_out = write_context.write_File(out_dir
+                                                    + this.test_name + '.dat');
+
+            console.log(this.test_name, bytes_out, 'bytes written');
+        },
+
+        on_read_complete: function(data) {
+            read_context.dv = new DataView(tools.toArrayBuffer(data));
+            read_context.cur_pos = 0;
+
+            Test_maps_B.hh = vp.read_Header(1, read_context);
+            Test_maps_B.aa = vp.read_OuterB(1, read_context);
+
+            console.log(Test_maps_B.test_name, data.length, 'bytes read');
+
+            Test_maps_B.validate();
+        }
+    };
+
+    vp.factory = persist // set factory
+
+    Test_maps_B.serialize();
+
+    tools.read_File(out_dir + Test_maps_B.test_name + '.dat',
+                                Test_maps_B.on_read_complete);
+})();
