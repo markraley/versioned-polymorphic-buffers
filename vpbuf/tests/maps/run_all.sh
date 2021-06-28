0a0b0cf715b2dde9d1297c5908dccf64dd6f35c5
markraley <mdraley2021@gmail.com>
2021-06-28 10:36:35 -0700

    test group scripts updated and tested

--- vpbuf/tests/maps/run_all.sh
+++ vpbuf/tests/maps/run_all.sh
@@ -4,19 +4,19 @@ pushd .
 
 echo 'running python tests'
 cd python
-python3 ./vectors.py
+python3 ./maps.py
 cd ..
 echo
 
 echo 'running nodejs tests'
 cd nodejs
-nodejs ./src/vectors.js
+nodejs ./src/maps.js
 cd ..
 echo
 
 echo 'running cplusplus tests'
 cd cpp
-./build/vectors
+./build/maps
 cd ..
 echo
 
